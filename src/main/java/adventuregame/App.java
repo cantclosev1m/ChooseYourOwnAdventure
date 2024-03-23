7d1b9ea4a9fffd5cde92e591ac4e894553100f74
Paul Choe <pyc0004@uah.edu>
2024-03-23 14:03:32 -0500

    moved main menu init herea

--- src/main/java/adventuregame/App.java
+++ src/main/java/adventuregame/App.java
@@ -7,6 +7,9 @@ import java.lang.StringBuilder;
 import java.util.List;  
 
 
+import javax.swing.*;
+import adventuregame.MainMenu;
+import adventuregame.EventScreen;
 
 /**
  * Hello world!
@@ -45,18 +48,21 @@ public class App
     {
         
     }
-    
-    /**
-     * Function for building and instance of the game menu
-     */
-    public void buildMenu()
-    {
-        
-    }
-    
 
     public static void main( String[] args ) throws Exception
     {
-
+        // display main menu
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                
+                MainMenu mm = new MainMenu();
+                mm.createMM();
+                
+                /* for testing purposes 
+                EventScreen eScreen = new EventScreen();
+                eScreen.buildEventScreen();
+                */
+            }
+        });
     }
 }
