4d932b29d24a216c2000e8054834b33bfdb7164a
rockr <ece0009@uah.edu>
2024-03-24 23:51:21 -0500

    Added new events to JSON file, including a dead-end event

--- src/main/java/adventuregame/Game.java
+++ src/main/java/adventuregame/Game.java
@@ -1,7 +1,39 @@
+package adventuregame;
+import adventuregame.Graph;
+
+public class Game{
 
 
+    /**
+     * getNextDescription
+     * function to return the description of the next node (Not even sure if it's useful).
+     * @param gameGraph
+     * @param currentNode
+     * @param choiceIndex
+     * @return
+     */
+     public String getNextDescription(Graph gameGraph, Graph.Node currentNode, int choiceIndex)
+    {
+        return gameGraph.nextNode(currentNode, choiceIndex).getDescription();
+    }
 
-public class Game
-{
+    /**
+     * updateGraphNode
+     * Function to update game position (will need alterations to account for UI implementation)
+     * @param gameGraph
+     * @param currentNode
+     * @param choiceIndex
+     * @return
+     */
+    public Graph.Node updateGraphNode(Graph gameGraph, Graph.Node currentNode, int choiceIndex)
+    {
+        Graph.Node nextNode = gameGraph.nextNode(currentNode, choiceIndex);
+        currentNode = nextNode;
+        return currentNode;
+    }
+    //TODO Implement function to check if game should be over.
+    public void checkGameOver()
+    {
 
+    }
 }
