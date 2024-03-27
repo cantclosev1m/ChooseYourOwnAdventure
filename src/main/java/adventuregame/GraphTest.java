67a161d2a3e21b0861daaf142d802c6a31080da4
rockr <ece0009@uah.edu>
2024-03-26 20:25:47 -0500

    Added GraphTest class

---
+++ src/main/java/adventuregame/GraphTest.java
@@ -0,0 +1,19 @@
+package adventuregame;
+
+import java.io.IOException;
+
+/**
+ * GraphTest
+ * Class for testing the graph class
+ */
+
+public class GraphTest {
+
+    public static void main(String[] args) throws IOException {
+        Graph myGraph = new Graph("game.json");
+        myGraph.initialize();
+
+    }
+
+
+}
