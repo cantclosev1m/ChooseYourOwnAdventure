9cfced9f35380b5f47182ff6a3e421d2ded07278
Paul Choe <pyc0004@uah.edu>
2024-04-18 19:43:14 -0500

    end game framework

---
+++ src/main/java/adventuregame/gui/EndingMenu.java
@@ -0,0 +1,67 @@
+package adventuregame.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class EndingMenu extends JFrame {
+    private JPanel panel;
+    private JLabel endgameLabel;
+    private JButton mmButton; // mm = main menu
+    private JButton exitButton;
+
+    public EndingMenu() {
+        setTitle("End");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // organization
+        panel = new JPanel(new GridBagLayout()); 
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(20, 0, 20, 0); // vertical spacer
+        gbc.anchor = GridBagConstraints.CENTER;
+
+        endgameLabel = new JLabel("<html>Game Over<br>This should be updated based on if the player won or not.<br>Maybe have a custom message based on what happens</html>");
+        endgameLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        
+        // end game button
+        exitButton = new JButton("Exit");
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        
+        // go back to main menu button
+        mmButton = new JButton("Main Menu");
+        mmButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //
+            }
+        });
+        
+        // more organization
+        panel.add(endgameLabel, gbc);
+        gbc.gridy++;
+        panel.add(mmButton, gbc);
+        gbc.gridy++;
+        panel.add(exitButton, gbc);
+        add(panel);
+    }
+
+    public void setVisibility(boolean b, Point coord) {
+        setLocation(coord); // make new window in the same location as game menu screen
+        setVisible(b);
+    }
+    
+    public static void main(String[] args) {
+        EndingMenu m = new EndingMenu();
+        m.setVisibility(true, new Point(100, 100));
+    }
+}
